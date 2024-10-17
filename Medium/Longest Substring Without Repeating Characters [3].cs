a04dfc61d3be8d03f7af3b47dde41b08d8704a3b
Beau Saunders <beaujacksaunders@gmail.com>
2024-10-17 14:14:34 +0100

    Create Longest Substring Without Repeating Characters [3].cs

---
+++ Medium/Longest Substring Without Repeating Characters [3].cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        if (string.IsNullOrEmpty(s)) return 0;
+        if (s.Length == 1) return 1;
+
+        //Can't have duplicates in HashSet
+        HashSet<char> chars = new HashSet<char>();
+
+        int current = 0;
+        int max = 0;
+        int left = 0;
+        //Increment right pointer every loop
+        for (int right = 0; right < s.Length; right++){
+            //While char at right pointer already exists in HashSet
+            while (!chars.Add(s[right])){
+                //Increment left point and remove the left pointer char from the HashSet
+                chars.Remove(s[left]);
+                left++;
+            }
+            //The right pointer char is now unique
+
+            //Current length of dynamic sliding window
+            current = right - left + 1;
+
+            //Update max length so far if larger
+            if (current > max) max = current;
+        }
+
+        return max;
+    }
+}
