81d3b56ff858ebd6085968a5027668c1d4d48080
Beau Saunders <beaujacksaunders@gmail.com>
2024-10-17 14:25:31 +0100

    Create Two Sum II - Input Array Is Sorted [167].cs

---
+++ Medium/Two Sum II - Input Array Is Sorted [167].cs
@@ -0,0 +1,31 @@
+/*
+Last Solved: 09.10.24
+Topics Utilised: Two Pointers
+Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+*/
+
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while (left < right){
+            int sum = numbers[left] + numbers[right];
+
+            //Sum of two values = target, return index of values in 1-indexed format
+            if (sum == target)
+                return new int[] {left + 1, right + 1};
+
+            //Sum is not enough, increment left pointer (increase sum)
+            if (sum < target)
+                left++;
+
+            //Sum is too much, decrease right pointer (decrease sum)
+            else
+                right--;
+        }
+
+        //No 2 indexes were found that add up to the target
+        return null;
+    }
+}
