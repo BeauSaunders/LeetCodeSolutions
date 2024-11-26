595a02cbf5fc6f0c13ec49277d67ab004f5989c3
Beau Saunders <beaujacksaunders@gmail.com>
2024-11-26 22:25:28 +0000

    Create [1768] Merge Strings Alternately.cs

---
+++ [1600-1799]/[1768] Merge Strings Alternately.cs
@@ -0,0 +1,36 @@
+/*
+Last Solved: 29.11.24
+Topics Utilised: String Builder, Loops
+Link: https://leetcode.com/problems/merge-strings-alternately/
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        StringBuilder sb = new StringBuilder();
+        int w2Len = word2.Length;
+        int i = 0;
+
+        // for each letter in word1
+        for (; i < word1.Length; i++)
+        {
+            sb.Append(word1[i]);
+
+            // if word2 has a letter at i
+            if (i < w2Len)
+            {
+                sb.Append(word2[i]);
+            }
+        }
+
+        // while there are letters remaining in word2
+        while (i < w2Len)
+        {
+            sb.Append(word2[i]);
+            i++;
+        }
+
+        return sb.ToString();
+    }
+}
