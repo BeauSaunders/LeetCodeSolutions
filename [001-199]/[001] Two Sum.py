7997ab1e857829df3f897dd781b51ec5b3130de6
Beau Saunders <beaujacksaunders@gmail.com>
2026-01-04 22:46:52 +0000

    Refactor twoSum method with type hints and optimizations
    Refactor twoSum method to use type hints and improve variable naming.

--- [001-199]/[001] Two Sum.py
+++ [001-199]/[001] Two Sum.py
@@ -4,25 +4,19 @@ Time Complexity: O(n)
 Space Complexity: O(n)
 """
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        # track seen ints
-        tracker = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        # for each int in nums
-        for i in range(len(nums)):
-            # number needed to find
-            need = target - nums[i]
+        numsDict = {}
+        numsLen = len(nums)
+        # for each num in nums
+        for i in range(numsLen):
+            remainder = target - nums[i]
 
-            # if it is in tracker
-            if need in tracker:
-                return [tracker[need], i]
-            # else, track this number and index
+            # if dict contains target - num
+            if remainder in numsDict:
+                # return curIndex, dict[num]
+                return [i, numsDict[remainder]]
+            # else remainder not in dict, add this num to the dict
             else:
-                tracker[nums[i]] = i
+                numsDict[nums[i]] = i
