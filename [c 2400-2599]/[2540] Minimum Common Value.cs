db97d56ccb611e8d22876035be5205a63586266c
Beau Saunders <beaujacksaunders@gmail.com>
2026-05-19 15:21:18 +0100

    Update [2540] Minimum Common Value.cs

--- [c 2400-2599]/[2540] Minimum Common Value.cs
+++ [c 2400-2599]/[2540] Minimum Common Value.cs
@@ -12,7 +12,6 @@ public class Solution {
 
         for (int i = 0; i < nums2.Length; i++)
         {
-            Console.WriteLine(nums2[i]);
             if (n1.Contains(nums2[i]))
                 return nums2[i];
         }
