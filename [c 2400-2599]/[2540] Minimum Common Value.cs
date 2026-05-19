7b3a4ed8b85aad2754d2ba6282c811b67a6a6aef
Beau Saunders <beaujacksaunders@gmail.com>
2026-05-19 15:17:30 +0100

    Create [2540] Minimum Common Value.cs

---
+++ [c 2400-2599]/[2540] Minimum Common Value.cs
@@ -0,0 +1,22 @@
+/*
+Last Solved: 19.05.26
+Topics Utilised: HashSet
+Link: https://leetcode.com/problems/minimum-common-value/
+Time Complexity: O(n + m) [where n = length of nums1 & m = length of nums 2]
+Space Complexity: O(n) [where n = length of nums1]
+*/
+
+public class Solution {
+    public int GetCommon(int[] nums1, int[] nums2) {
+        HashSet<int> n1 = new(nums1);
+
+        for (int i = 0; i < nums2.Length; i++)
+        {
+            Console.WriteLine(nums2[i]);
+            if (n1.Contains(nums2[i]))
+                return nums2[i];
+        }
+
+        return -1;
+    }
+}
